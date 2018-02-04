5fa2c0f4c8638be4fda653518f21296ac0a6c3b0
Method <git@zanuka.com>
2018-02-04 14:30:39 +1300

    mapped app state to props of ItemDetail component/container

--- src/containers/item-detail.js
+++ src/containers/item-detail.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class ItemDetail extends Component {
+class ItemDetail extends Component {
   render() {
     return (
         <div>Item Detail</div>
       )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    item: state.activeItem
+  }
+}
+
+export default connect(mapStateToProps)(ItemDetail)
